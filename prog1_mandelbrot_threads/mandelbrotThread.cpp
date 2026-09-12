1c4468c74a35f0c2661bc4ed7eb4c252fe7c4d40
abdul-rauf <irauf.babar@gmail.com>
2026-09-12 16:11:32 +0500

    Implement block decomposition for Program 1

--- prog1_mandelbrot_threads/mandelbrotThread.cpp
+++ prog1_mandelbrot_threads/mandelbrotThread.cpp
@@ -35,8 +35,26 @@ void workerThreadStart(WorkerArgs * const args) {
     // to compute a part of the output image.  For example, in a
     // program that uses two threads, thread 0 could compute the top
     // half of the image and thread 1 could compute the bottom half.
+    int rowsPerThread = args->height / args->numThreads;
+    int startRow = args->threadId * rowsPerThread;
+    int numRows = rowsPerThread;
 
-    printf("Hello world from thread %d\n", args->threadId);
+    if (args->threadId == args->numThreads - 1) {
+        numRows = args->height - startRow;
+    }
+
+    mandelbrotSerial(
+        args->x0,
+        args->y0,
+        args->x1,
+        args->y1,
+        args->width,
+        args->height,
+        startRow,
+        numRows,
+        args->maxIterations,
+        args->output
+    );
 }
 
 //
